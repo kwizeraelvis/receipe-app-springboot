456aa6b97c1547ec9b55cb6dd5c626d44faa32b8
Kwizera Aime Elvis <kwizeraelvis6@gmail.com>
2020-01-31 20:14:01 +0200

    Project BootStrap

---
+++ src/test/java/com/elvis/springapp/springrecipe/SpringRecipeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elvis.springapp.springrecipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRecipeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
