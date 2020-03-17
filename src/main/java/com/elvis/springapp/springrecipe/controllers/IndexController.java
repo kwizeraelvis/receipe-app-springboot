65dc5bd104be9cb08518945dee1d5fd4a5dd0d6a
Kwizera Aime Elvis <kwizeraelvis6@gmail.com>
2020-03-17 16:22:16 +0200

    Remove uncessary fields

--- src/main/java/com/elvis/springapp/springrecipe/controllers/IndexController.java
+++ src/main/java/com/elvis/springapp/springrecipe/controllers/IndexController.java
@@ -1,10 +1,12 @@
 package com.elvis.springapp.springrecipe.controllers;
 
+import com.elvis.springapp.springrecipe.Service.RecipeService;
 import com.elvis.springapp.springrecipe.domain.Category;
 import com.elvis.springapp.springrecipe.domain.UnitOfMeasure;
 import com.elvis.springapp.springrecipe.repositories.CategoryRepository;
 import com.elvis.springapp.springrecipe.repositories.UnitOfMeasureRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Optional;
@@ -12,22 +14,16 @@ import java.util.Optional;
 @Controller
 public class IndexController {
 
-    private CategoryRepository categoryRepository;
-    private UnitOfMeasureRepository unitOfMeasureRepository;
+    private RecipeService recipeService;
 
 
-    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
-        this.categoryRepository = categoryRepository;
-        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    public IndexController(RecipeService recipeService) {
+        this.recipeService = recipeService;
     }
 
     @RequestMapping({"", "/", "/index"})
-    public String getIndexPage(){
-        Optional<Category> categoryOptional = categoryRepository.findByDescription("AMERICAN");
-        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Ounce");
-
-        System.out.println("Categoru Id is "  + categoryOptional.get().getId());
-        System.out.println("UnitOfMeasure Id is " + unitOfMeasureOptional.get().getId());
+    public String getIndexPage(Model model){
+        model.addAttribute("recepies", recipeService.getRecipes());
         return "index";
     }
 }
