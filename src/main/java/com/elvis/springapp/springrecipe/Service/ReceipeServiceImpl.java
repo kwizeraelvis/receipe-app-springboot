d13a075c5aa701ff0bccc1957b9e168947153748
Kwizera Aime Elvis <kwizeraelvis6@gmail.com>
2020-08-16 17:15:46 +0200

    Define and Implement recipe service

--- src/main/java/com/elvis/springapp/springrecipe/Service/ReceipeServiceImpl.java
+++ src/main/java/com/elvis/springapp/springrecipe/Service/ReceipeServiceImpl.java
@@ -1,11 +1,16 @@
 package com.elvis.springapp.springrecipe.Service;
 
+import com.elvis.springapp.springrecipe.commands.RecipeCommand;
+import com.elvis.springapp.springrecipe.converters.RecipeCommandToRecipe;
+import com.elvis.springapp.springrecipe.converters.RecipeToRecipeCommand;
 import com.elvis.springapp.springrecipe.domain.Recipe;
 import com.elvis.springapp.springrecipe.repositories.RecipeRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 
 @Slf4j
@@ -13,9 +18,13 @@ import java.util.Set;
 public class ReceipeServiceImpl implements RecipeService {
 
     private final RecipeRepository recipeRepository;
+    private final RecipeCommandToRecipe recipeCommandToRecipe;
+    private final RecipeToRecipeCommand recipeToRecipeCommand;
 
-    public ReceipeServiceImpl(RecipeRepository recipeRepository) {
+    public ReceipeServiceImpl(RecipeRepository recipeRepository, RecipeCommandToRecipe recipeCommandToRecipe, RecipeToRecipeCommand recipeToRecipeCommand) {
         this.recipeRepository = recipeRepository;
+        this.recipeCommandToRecipe = recipeCommandToRecipe;
+        this.recipeToRecipeCommand = recipeToRecipeCommand;
     }
 
     @Override
@@ -24,4 +33,32 @@ public class ReceipeServiceImpl implements RecipeService {
         recipeRepository.findAll().iterator().forEachRemaining(recipes::add);
         return recipes;
     }
+
+    @Override
+    public Recipe findById(Long id) {
+        Optional<Recipe> recipeOptional = recipeRepository.findById(id);
+        if(!recipeOptional.isPresent()){
+            throw new RuntimeException("Receipe Not Found");
+        }
+        return recipeOptional.get();
+    }
+
+    @Override
+    @Transactional
+    public RecipeCommand saveRecipeCommand(RecipeCommand command) {
+        Recipe detachedRecipe = recipeCommandToRecipe.convert(command);
+        Recipe savedRecipe = recipeRepository.save(detachedRecipe);
+        return recipeToRecipeCommand.convert(savedRecipe);
+    }
+
+	@Override
+	@Transactional
+	public RecipeCommand findCommandById(Long id) {
+		return recipeToRecipeCommand.convert(findById(id));
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		recipeRepository.deleteById(id);		
+	}
 }
--- src/main/java/com/elvis/springapp/springrecipe/Service/RecipeService.java
+++ src/main/java/com/elvis/springapp/springrecipe/Service/RecipeService.java
@@ -1,5 +1,6 @@
 package com.elvis.springapp.springrecipe.Service;
 
+import com.elvis.springapp.springrecipe.commands.RecipeCommand;
 import com.elvis.springapp.springrecipe.domain.Recipe;
 import org.springframework.stereotype.Service;
 
@@ -8,4 +9,8 @@ import java.util.Set;
 
 public interface RecipeService {
     Set<Recipe> getRecipes();
+    Recipe findById(Long id);
+    RecipeCommand saveRecipeCommand(RecipeCommand command);
+    RecipeCommand findCommandById(Long id);
+    void deleteById(Long id);
 }
