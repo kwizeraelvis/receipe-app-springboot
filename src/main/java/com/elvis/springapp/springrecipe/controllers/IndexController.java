15a7b3d782377853cd402f05ed07f10bdbea0eda
Kwizera Aime Elvis <kwizeraelvis6@gmail.com>
2020-08-16 17:09:38 +0200

    Fixed typo in model attributes

--- src/main/java/com/elvis/springapp/springrecipe/controllers/IndexController.java
+++ src/main/java/com/elvis/springapp/springrecipe/controllers/IndexController.java
@@ -1,21 +1,13 @@
 package com.elvis.springapp.springrecipe.controllers;
 
 import com.elvis.springapp.springrecipe.Service.RecipeService;
-import com.elvis.springapp.springrecipe.domain.Category;
-import com.elvis.springapp.springrecipe.domain.UnitOfMeasure;
-import com.elvis.springapp.springrecipe.repositories.CategoryRepository;
-import com.elvis.springapp.springrecipe.repositories.UnitOfMeasureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.Optional;
-
 @Controller
 public class IndexController {
-
     private RecipeService recipeService;
-
 
     public IndexController(RecipeService recipeService) {
         this.recipeService = recipeService;
@@ -23,7 +15,9 @@ public class IndexController {
 
     @RequestMapping({"", "/", "/index"})
     public String getIndexPage(Model model){
-        model.addAttribute("recepies", recipeService.getRecipes());
+        model.addAttribute("recipes", recipeService.getRecipes());
         return "index";
     }
+
+
 }
