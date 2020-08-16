f9cb14a029b018b68e7ddf82d2a83a20d9674612
Kwizera Aime Elvis <kwizeraelvis6@gmail.com>
2020-08-16 17:10:40 +0200

    Implement Bi-Directional Converters for the Category and Category Command

---
+++ src/main/java/com/elvis/springapp/springrecipe/converters/CategoryToCategoryCommand.java
@@ -0,0 +1,25 @@
+package com.elvis.springapp.springrecipe.converters;
+
+import com.elvis.springapp.springrecipe.commands.CategoryCommand;
+import com.elvis.springapp.springrecipe.domain.Category;
+import lombok.Synchronized;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.lang.Nullable;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CategoryToCategoryCommand implements Converter<Category, CategoryCommand> {
+
+    @Synchronized
+    @Nullable
+    @Override
+    public CategoryCommand convert(Category source) {
+        if(source == null){
+            return null;
+        }
+        final CategoryCommand categoryCommand = new CategoryCommand();
+        categoryCommand.setId(source.getId());
+        categoryCommand.setDescription(source.getDescription());
+        return categoryCommand;
+    }
+}
